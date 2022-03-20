61dd6ef3be83f5c394e81c76f14debe79d1adbbd
thekrishnarajput <mukeshkarn1994@gmail.com>
2022-03-20 23:50:14 +0530

    new

--- controller/customer/customerProfileController.js
+++ controller/customer/customerProfileController.js
@@ -5,15 +5,14 @@ const { validationResult } = require('express-validator')
 const profile = require('../../model/customer/customerProfileModel')
 
 exports.Profile = (request, response) => {
-    profile.updateOne({
-        customers: request.body.customerId,
+    profile.updateOne({customers: request.body.customerId}, {$set:{
         address1: request.body.address1,
         address2: request.body.address2,
         address3: request.body.address3,
         profilePic: "https://bhukkad-hub.herokuapp.com/customer/media" + request.file.filename,
         location: request.body.location,
         bio: request.body.bio
-    })
+    }})
         .then(result => {
             console.log("Result in profile: ", result)
             return response.status(200).json(result)
