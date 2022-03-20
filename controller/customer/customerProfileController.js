3702b41216d351bcb7b25757913b28ce1092b0e0
thekrishnarajput <mukeshkarn1994@gmail.com>
2022-03-20 23:38:03 +0530

    test-update

--- controller/customer/customerProfileController.js
+++ controller/customer/customerProfileController.js
@@ -6,11 +6,11 @@ const profile = require('../../model/customer/customerProfileModel')
 
 exports.Profile = (request, response) => {
     profile.updateOne({
-        customers: request.params.customerId,
+        customers: request.body.customerId,
         address1: request.body.address1,
         address2: request.body.address2,
         address3: request.body.address3,
-        profilePic: "https://bhukkad-hub.herokuapp.com/" + request.file.filename,
+        profilePic: "https://bhukkad-hub.herokuapp.com/customer/media" + request.file.filename,
         location: request.body.location,
         bio: request.body.bio
     })
