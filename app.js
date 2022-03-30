a78f5039735821a00d9ed4a50290f60693595080
thekrishnarajput <mukeshkarn1994@gmail.com>
2022-03-30 08:38:03 +0530

    cors edit

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const adminRoute = require('./routes/admin/adminRoute')
 const customerRoute = require('./routes/customer/customerRoute')
 const dishRoute = require('./routes/admin/dishRoute')
 
-app.use(cors())
+app.use(cors({
+    origin: '*'
+}))
 app.use(express.json({ limit: "50mb" }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extended: true}))
